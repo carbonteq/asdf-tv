da44da7a8100ab4d0d57b62bda76fca2bc09243e
Arslan <rslnkrmt2552@gmail.com>
2022-09-13 18:08:38 +0500

    add sed transform to remove ref based tags from list-all

--- lib/utils.bash
+++ lib/utils.bash
@@ -26,7 +26,7 @@ sort_versions() {
 list_github_tags() {
   git ls-remote --tags --refs "$GH_REPO" |
     grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//'
+    sed 's/^v//' | sed -E 's/refs\/[a-zA-Z\/]*//g'
 }
 
 list_all_versions() {
@@ -39,7 +39,9 @@ get_latest_version() {
 
   latest_version=$(curl -L --silent "${curl_opts[@]}" "https://api.github.com/repos/alexhallam/tv/releases/latest" |
     grep '"tag_name":' |
-    sed -E 's/.*"([^"]+)".*/\1/')
+    sed -E 's/.*"([^"]+)".*/\1/') # |
+  # sed -E 's/refs\/[a-zA-Z\/]*//g')
+
   echo "Latest version for tidy-viewer is $latest_version" >&2
 
   echo "$latest_version"
